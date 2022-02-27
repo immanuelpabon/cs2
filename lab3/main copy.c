597c88118f9a9a066f514033a9906c9e6e505ae6
Immanuel Pabon <immanuel@chrysalix.org>
2022-02-27 13:18:33 -0500

    lab3 checkpoint2

--- lab3/main copy.c
+++ lab3/main copy.c
@@ -8,13 +8,15 @@ int main(int argc, char* argv[])
    MY_STRING hMy_string = NULL;
    FILE* fp;
    hMy_string = my_string_init_default();
-   fp = fopen("dictionary.txt", "r");
+   fp = fopen("simple.txt", "r");
 
    while(my_string_extraction(hMy_string, fp))
    {
-      if (my_string_get_size(hMy_string) == 29) {
-         my_string_insertion(hMy_string, stdout);
-         printf("\n");
+      my_string_insertion(hMy_string, stdout);
+      printf("\n");
+      if(fgetc(fp)== ' ')
+      {
+         printf("Found a space after the string\n");
       }
    }
 
